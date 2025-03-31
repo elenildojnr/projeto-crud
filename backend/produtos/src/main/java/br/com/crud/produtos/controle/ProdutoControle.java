4c403977db5bbe783df565ebed1286b6b8aa7007
jabes <jabesvieira4@gmail.com>
2025-03-31 14:33:46 -0300

    Alteração do metodo cadastrar

--- backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
@@ -1,10 +1,17 @@
 package br.com.crud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crud.produtos.modelo.ProdutoModelo;
@@ -14,25 +21,36 @@ import br.com.crud.produtos.servico.ProdutoServico;
 
 public class ProdutoControle {
 
+  // Injeção de dependências
+
   @Autowired
   private ProdutoServico ps;
 
   @PostMapping("/cadastrar")
-
   public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
+    return ps.cadastrarAlterar(pm, "cadastrar");
+  }
 
-    return ps.cadastrar(pm);
+  @PutMapping("/alterar")
+
+  public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
+
+    return ps.cadastrarAlterar(pm, "alterar");
 
   }
 
   @GetMapping("/listar")
+
   public Iterable<ProdutoModelo> listar() {
+
     return ps.listar();
 
   }
 
   @GetMapping("/")
+
   public String rota() {
+
     return "A API está funcionando!";
 
   }
