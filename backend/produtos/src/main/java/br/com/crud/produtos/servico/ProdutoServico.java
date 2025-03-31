d0086f56f054c812cbb43e3d83985f086c93402a
jabes <jabesvieira4@gmail.com>
2025-03-31 14:19:00 -0300

    Criação da classe resposta modelo

--- backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
@@ -1,9 +1,7 @@
 package br.com.crud.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
-
 import br.com.crud.produtos.modelo.ProdutoModelo;
 import br.com.crud.produtos.repositorio.ProdutoRepositorio;
 
