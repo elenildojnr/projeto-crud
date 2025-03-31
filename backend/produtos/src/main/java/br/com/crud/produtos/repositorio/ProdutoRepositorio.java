ff8b6ec24673aeb82ac4476d0813b6838cd598c2
jabes <jabesvieira4@gmail.com>
2025-03-31 14:14:25 -0300

    criação do metodo listar() de servico

--- backend/produtos/src/main/java/br/com/crud/produtos/repositorio/ProdutoRepositorio.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/repositorio/ProdutoRepositorio.java
@@ -1,8 +1,11 @@
 package br.com.crud.produtos.repositorio;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import br.com.crud.produtos.modelo.ProdutoModelo;
 
+@Repository
 public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
 
 }
